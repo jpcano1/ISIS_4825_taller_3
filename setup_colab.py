6c756a982166535c5e8a7bd63047fc627460b550
Juan Pablo <jp.cano@uniandes.edu.co>
2020-08-20 15:40:55 -0500

    Funciones para ejecutar en colab

---
+++ setup_colab.py
@@ -0,0 +1,18 @@
+import os
+import requests
+
+def download_github_content(path):
+    dir = path.rsplit("/", 1)[0]
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        os.system(f"touch {dir}/__init__.py")
+
+    elif not os.path.exists(f"{dir}/__init__.py"):
+        os.system(f"touch {dir}/__init__.py")
+
+    os.system(f"shred -u {path}")
+    os.system(f"wget https://raw.githubusercontent.com/jpcano1/ISIS_4825_taller_3/master/{path} -O {path}")
+
+def setup_workshop():
+    download_github_content("utils/color_segmentation.py")
+    download_github_content("utils/visualization.py")
