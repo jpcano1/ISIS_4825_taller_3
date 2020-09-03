db73e5726966f18e3a702e1439cd56e012860d0c
Juan Pablo <jp.cano@uniandes.edu.co>
2020-09-02 23:38:45 -0500

    Bash Installing

---
+++ unix_install_environment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ -z "$ENV" ]; then
+  echo "No Existe, por favor crea esta variable"
+else
+  if [ ! -d "$ENV" ]
+  then
+    pip install virtualenv
+    virtualenv $ENV
+    source $ENV/bin/activate
+    pip install -r requirements.txt
+  fi
+
+  source $ENV/bin/activate
+  if [[ ! $(pip list | grep -w "\<ipykernel\>") ]]; then
+    pip install ipykernel
+  fi
+  python -m ipykernel install --user --name=$ENV
+  jupyter notebook
+  echo y | jupyter kernelspec uninstall $ENV
+  deactivate
+fi
