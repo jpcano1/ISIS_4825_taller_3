5e5973c2ac8b10f486f10becd9710fbb255d145f
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2020-09-03 08:23:01 -0500

    Windows Batch Installing

---
+++ windows_install_environment.bat
@@ -0,0 +1,20 @@
+@echo off
+
+if "%ENV%"=="" (
+    echo Variable no definida
+) else (
+    if not exist "%cd%/%ENV%" (
+        pip install virtualenv
+        virtualenv "%ENV%"
+        "%ENV%/Scripts/activate"
+        pip install -r requirements.txt
+    )
+
+    "%ENV%/Scripts/activate"
+    python -m ipykernel install --user --name="%ENV%"
+    jupyter notebook
+    echo y | jupyter kernelspec uninstall "%ENV%"
+    "%ENV%%/Scripts/deactivate"
+)
+
+PAUSE
